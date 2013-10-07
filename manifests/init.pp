296da3451ded36470826956406bd73d6b58e690e
Renaud Tircher <renaud.tircher@gmail.com>
2013-10-07 08:17:00 -0700

    Updating to latest onepassword

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,7 @@
 #     include 1password
 class onepassword {
   $host = 'd13itkw33a7sus.cloudfront.net'
-  $path = 'dist/1P/mac/1Password-3.8.21.zip'
+  $path = 'dist/1P/mac4/1Password-4.0.1.zip'
 
   package { '1Password':
     source   => "https://${host}/${path}",
